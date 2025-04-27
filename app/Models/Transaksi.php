1e48c8b3ece115ad15c92d987fa5d78f80794cf5
Riskyy-06 <aliffianrizki6@gmail.com>
2025-04-27 20:31:47 +0700

    Menambahkan menu navigasi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
     protected $fillable = ['total_harga'];
+
+    use HasFactory;
 
     public function items()
     {
